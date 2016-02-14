2dc6474af2e6c4e2dc77f25e514eeaa608c5e43e
Robin Kalia <robin.kalia@daqri.com>
2016-02-13 20:06:10 -0800

    Added the GNU GPL terms to the source code files. Also, the version includes the "StringMatcher.sln" file that was built using Visual Studio 2013 and is compatible with same or greater versions of Visual Studio only. I will very soon add the 'Makefile' that will help programmers to compile the code on UNIX and Linux platforms.

--- Version 1.0 - Beta/String Matching/StringMatcher/StringMatcher.h
+++ Version 1.0 - Beta/String Matching/StringMatcher/StringMatcher.h
@@ -1,4 +1,25 @@
+// Robin Kalia
+// robinkalia@berkeley.edu
+// Project: String Matching
+// 
+// StringMatcher.h: Contains the declaration of different methods and data members used in the "StringMatcher" class that
+//                  provides access to different efficient string matching algorithms.    
 
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <iostream>
 
